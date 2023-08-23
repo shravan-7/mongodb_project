a8d5d90674c0cdeb8913d1f0193ec040a5449abb
shravan kumar <skcode75@gmail.com>
2023-08-23 19:50:08 +0530

    chagnes

--- application/views/user_update.php
+++ application/views/user_update.php
@@ -5,12 +5,13 @@
 
 <head>
     <meta charset="utf-8">
+    <title>Update User</title>
 
-    <!-- Include the Flatly Bootswatch CSS -->
-    <link rel="stylesheet" type="text/css" href="https://bootswatch.com/5/flatly/bootstrap.min.css">
+    <!-- Include the Bootstrap CSS link -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
     <style>
-        /* Center content both vertically and horizontally */
+        /* Center content vertically and horizontally */
         body {
             display: flex;
             justify-content: center;
@@ -18,60 +19,66 @@
             height: 100vh;
         }
 
-        /* Add additional styles for form container */
-        #container {
-            text-align: center;
+        /* Adjust the max-width of the card container */
+        .card-container {
+            max-width: 400px;
+        }
+
+        /* Increase the width of the input fields */
+        .form-control-custom {
+            width: 100%;
         }
     </style>
 </head>
 
 <body>
-    <div id="container">
-        <h1>Update User Details</h1>
+    <div class="card card-container">
+        <div class="card-body">
+            <h1 class="text-center">Update User Details</h1>
 
+            <div id="body">
+                <?php
+                if (isset($error)) {
+                    echo '<p style="color:red;">' . $error . '</p>';
+                } else {
+                    echo validation_errors();
+                }
+                ?>
 
+                <?php
+                $attributes = array('name' => 'form', 'id' => 'form', 'class' => 'form');
+                echo form_open($this->uri->uri_string(), $attributes);
+                ?>
 
-        <div id="body" style="max-width: 500px;  ">
-            <?php
-            if (isset($error)) {
-                echo '<p style="color:red;">' . $error . '</p>';
-            } else {
-                echo validation_errors();
-            }
-            ?>
+                <div class="mb-3">
+                    <label for="name" class="form-label">Full Name</label>
+                    <input type="text" name="name" class="form-control-custom" value="<?php echo isset($user->name) ? $user->name : set_value('name'); ?>" required>
+                </div>
 
-            <?php
-            $attributes = array('name' => 'form', 'id' => 'form', 'class' => 'form');
-            echo form_open($this->uri->uri_string(), $attributes);
-            ?>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email Address</label>
+                    <input type="email" name="email" class="form-control-custom" value="<?php echo isset($user->email) ? $user->email : set_value('email'); ?>" required>
+                </div>
 
-            <div class="mb-3">
-                <label for="name" class="form-label">Full Name</label>
-                <input type="text" name="name" class="form-control" value="<?php echo isset($user->name) ? $user->name : set_value('name'); ?>" required>
-            </div>
+                <div class="mb-3">
+                    <label for="gender" class="form-label">Gender</label>
+                    <select name="gender" class="form-control-custom" required>
+                        <option value="male" <?php echo set_select('gender', 'male', isset($user->gender) && $user->gender == 'male'); ?>>Male</option>
+                        <option value="female" <?php echo set_select('gender', 'female', isset($user->gender) && $user->gender == 'female'); ?>>Female</option>
+                        <option value="other" <?php echo set_select('gender', 'other', isset($user->gender) && $user->gender == 'other'); ?>>Other</option>
+                    </select>
+                </div>
 
-            <div class="mb-3">
-                <label for="email" class="form-label">Email Address</label>
-                <input type="email" name="email" class="form-control" value="<?php echo isset($user->email) ? $user->email : set_value('email'); ?>" required>
-            </div>
+                <div class="mb-3">
+                    <label for="mobile" class="form-label">Mobile Number</label>
+                    <input type="text" name="mobile" class="form-control-custom" value="<?php echo isset($user->mobile) ? $user->mobile : set_value('mobile'); ?>" required>
+                </div>
 
-            <div class="mb-3">
-                <label for="gender" class="form-label">Gender</label>
-                <select name="gender" class="form-select" required>
-                    <option value="male" <?php echo set_select('gender', 'male', isset($user->gender) && $user->gender == 'male'); ?>>Male</option>
-                    <option value="female" <?php echo set_select('gender', 'female', isset($user->gender) && $user->gender == 'female'); ?>>Female</option>
-                    <option value="other" <?php echo set_select('gender', 'other', isset($user->gender) && $user->gender == 'other'); ?>>Other</option>
-                </select>
-            </div>
-
-            <div class="mb-3">
-                <label for="mobile" class="form-label">Mobile Number</label>
-                <input type="text" name="mobile" class="form-control" value="<?php echo isset($user->mobile) ? $user->mobile : set_value('mobile'); ?>" required>
-            </div>
-
-            <div class="mb-3 d-flex justify-content-between align-items-center">
-                <input type="submit" name="submit" value="Submit" class="btn btn-success" style="margin-right: 10px;" />
-                <a href="/usercontroller" class="btn btn-primary">Back to Users</a>
+                <div class="mb-3 d-flex justify-content-between align-items-center">
+                    <input type="submit" name="submit" value="Submit" class="btn btn-success" style="margin-right: 10px;" />
+                    <a href="<?php echo base_url(); ?>/usercontroller" class="btn btn-primary">Back to Users</a>
+                </div>
+                </form>
             </div>
         </div>
     </div>
